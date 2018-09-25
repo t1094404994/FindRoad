b9cc98c61007e6aa7cbdaeb98e4563cef3accb84
t1094404994 <t1094404@live.com>
2018-09-25 19:22:57 +0800

    some change

--- bin-debug/backgroundScene.js
+++ bin-debug/backgroundScene.js
@@ -18,6 +18,7 @@ var backgroundScene = (function (_super) {
         var _this = _super.call(this) || this;
         _this.shapeW = 50; //格子
         _this.shapeH = 50; //格子
+        _this.seletPtArr = [];
         _this.str1 = "new Pt is :";
         _this.str2 = ",";
         _this.str3 = "by TOUCH_BEGIN";
@@ -40,42 +41,65 @@ var backgroundScene = (function (_super) {
             for (var j = 0; j < lineNum; j++) {
                 backShap = new egret.Shape();
                 backShap.touchEnabled = false;
-                backShap.graphics.lineStyle(2, 0x454545);
-                backShap.graphics.beginFill(0x000000, 0.5);
-                backShap.graphics.drawRect(j * this.shapeW, i * this.shapeH, this.shapeW, this.shapeH);
-                backShap.graphics.endFill();
                 backShap.alpha = 0.5;
                 shap = new egret.Shape();
-                shap.touchEnabled = true;
-                shap.graphics.lineStyle(2, 0x454545);
-                shap.graphics.beginFill(0xFFFFFF, 1);
-                shap.graphics.drawRect(j * this.shapeW, i * this.shapeH, this.shapeW, this.shapeH);
-                shap.graphics.endFill();
                 shap["coordinateX"] = j;
                 shap["coordinateY"] = i;
+                shap["onChoice"] = false;
                 this.addChild(shap);
                 this.addChild(backShap);
                 this.shaps.push(shap);
                 this.backShaps.push(backShap);
+                var pass = Math.floor(Math.random() * 8) == 7; //1/8是障碍物
+                if (pass) {
+                    shap.touchEnabled = false;
+                    shap.graphics.lineStyle(2, 0x454545);
+                    shap.graphics.beginFill(0x919191, 1);
+                    shap.graphics.drawRect(j * this.shapeW, i * this.shapeH, this.shapeW, this.shapeH);
+                    shap.graphics.endFill();
+                    backShap.graphics.lineStyle(2, 0x919191);
+                    backShap.graphics.beginFill(0x000000, 0.5);
+                    backShap.graphics.drawRect(j * this.shapeW, i * this.shapeH, this.shapeW, this.shapeH);
+                    backShap.graphics.endFill();
+                }
+                else {
+                    shap.touchEnabled = true;
+                    shap.graphics.lineStyle(2, 0x454545);
+                    shap.graphics.beginFill(0xFFFFFF, 1);
+                    shap.graphics.drawRect(j * this.shapeW, i * this.shapeH, this.shapeW, this.shapeH);
+                    shap.graphics.endFill();
+                    backShap.graphics.lineStyle(2, 0x454545);
+                    backShap.graphics.beginFill(0x000000, 0.5);
+                    backShap.graphics.drawRect(j * this.shapeW, i * this.shapeH, this.shapeW, this.shapeH);
+                    backShap.graphics.endFill();
+                }
                 (function (shap, thisObj) {
-                    shap.addEventListener(egret.TouchEvent.TOUCH_BEGIN, thisObj.onBegin, thisObj);
-                    shap.addEventListener(egret.TouchEvent.TOUCH_END, thisObj.onEnd, thisObj);
+                    shap.addEventListener(egret.TouchEvent.TOUCH_TAP, thisObj.onEnd, thisObj);
                 }(shap, this));
             }
         }
     };
-    backgroundScene.prototype.onBegin = function (evt) {
-        var obj = evt.target;
-        this.backShaps[obj.coordinateX + obj.coordinateY * this.lineNum].alpha = 1;
-        console.log(this.str1 + obj.coordinateX + this.str2 + obj.coordinateY + this.str3);
-    };
     backgroundScene.prototype.onEnd = function (evt) {
         var obj = evt.target;
-        this.backShaps[obj.coordinateX + obj.coordinateY * this.lineNum].alpha = 0.5;
+        if (obj.onChoice) {
+            this.backShaps[obj.coordinateX + obj.coordinateY * this.lineNum].alpha = 0.5;
+            obj.onChoice = false;
+            this.seletPtArr.shift();
+        }
+        else {
+            this.checkArr();
+            obj.onChoice = true;
+            this.seletPtArr.push(obj.coordinateX + obj.coordinateY * this.lineNum);
+            this.backShaps[obj.coordinateX + obj.coordinateY * this.lineNum].alpha = 1;
+        }
         console.log(this.str1 + obj.coordinateX + this.str2 + obj.coordinateY + this.str4);
     };
-    backgroundScene.prototype.OnTap = function () {
-        console.log("ssssssssssssss");
+    backgroundScene.prototype.checkArr = function () {
+        if (this.seletPtArr.length >= 2) {
+            var pt = this.seletPtArr.shift();
+            this.backShaps[pt].alpha = 0.5;
+            this.backShaps[pt]["onChoice"] = false;
+        }
     };
     return backgroundScene;
 }(egret.DisplayObjectContainer));
