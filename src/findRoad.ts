6a579f3f176b8f89bb7ec0b4fc0fbd524f794ad4
t1094404994 <t1094404@live.com>
2018-09-26 19:10:11 +0800

    add findRoad

---
+++ src/findRoad.ts
@@ -0,0 +1,12 @@
+/**寻路算法
+ * 深度搜索
+*/
+class findRoad{
+    private _closeArr:number[]; //障碍
+    constructor(){
+        
+    }
+    public set closeArr(arr:number[]){
+        this._closeArr=arr;
+    } 
+}
